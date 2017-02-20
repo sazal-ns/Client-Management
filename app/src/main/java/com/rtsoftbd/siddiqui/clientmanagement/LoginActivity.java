09ac848f9b5a8feb3b71d8093cc99cef5091e94d
Sazal Siddiqui NS <sazal.siddiqui@gmail.com>
2017-02-20 17:55:08 +0600

    add login view

--- app/src/main/java/com/rtsoftbd/siddiqui/clientmanagement/LoginActivity.java
+++ app/src/main/java/com/rtsoftbd/siddiqui/clientmanagement/LoginActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
